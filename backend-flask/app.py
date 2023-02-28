d526a79d077d4247b46311fa88f2a6f175e37b67
chivondo <veral.jose@gmail.com>
2023-02-28 18:56:47 +0000

    add spans and attributes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,6 +21,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # X-RAY ---------------------
 from aws_xray_sdk.core import xray_recorder
@@ -45,6 +46,11 @@ LOGGER.info("test log")
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# Show this in the logs within the backend-flask app (STDOUT)
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
