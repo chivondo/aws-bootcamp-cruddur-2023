d545e7ac725dd1279f0cc2bd33fb58a950251043
chivondo <veral.jose@gmail.com>
2023-03-03 20:00:55 +0000

    Fix xrays-subsegments

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -151,6 +151,7 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run(Logger=LOGGER)
   return data, 200
@@ -161,6 +162,7 @@ def data_notifcations():
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
+@xray_recorder.capture('activities_users')
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
