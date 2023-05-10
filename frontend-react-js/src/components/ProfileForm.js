385de82a440b74c6f1f19ba4464f8d58fefff037
chivondo <veral.jose@gmail.com>
2023-05-10 20:30:01 +0000

    implementing upload

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -9,29 +9,32 @@ export default function ProfileForm(props) {
 
   React.useEffect(()=>{
     console.log('useEffects',props)
-    setBio(props.profile.bio);
+    setBio(props.profile.bio || '');
     setDisplayName(props.profile.display_name);
   }, [props.profile])
 
-  const s3upload = async (event) => {
-    console.log('s3upload')
-    event.preventDefault();
+  const s3uploadkey = async (extension) => {
+    console.log('ext',extension)
     try {
-      const backend_url = "https://bregvghzq1.execute-api.us-east-1.amazonaws.com/avatars/key_upload"
+      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
-      const res = await fetch(backend_url, {
+      const json = {
+        extension: extension
+      }
+      const res = await fetch(gateway_url, {
         method: "POST",
+        body: JSON.stringify(json),
         headers: {
-          'Origin': "https://3000-chivondo-awsbootcampcru-6o7mx9shd2j.ws-us97.gitpod.io",
+          'Origin': process.env.REACT_APP_FRONTEND_URL,
           'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
-        },
-      });
+        }
+      })
       let data = await res.json();
       if (res.status === 200) {
-        console.log('presigned url',data)
+        return data.url
       } else {
         console.log(res)
       }
@@ -39,6 +42,36 @@ export default function ProfileForm(props) {
       console.log(err);
     }
   }
+
+  const s3upload = async (event)=> {
+    console.log('event',event)
+    const file = event.target.files[0]
+    const filename = file.name
+    const size = file.size
+    const type = file.type
+    const preview_image_url = URL.createObjectURL(file)
+    console.log(filename,size,type)
+    const fileparts = filename.split('.')
+    const extension = fileparts[fileparts.length-1]
+    const presignedurl = await s3uploadkey(extension)
+    try {
+      console.log('s3upload')
+      const res = await fetch(presignedurl, {
+        method: "PUT",
+        body: file,
+        headers: {
+          'Content-Type': type
+      }})
+      if (res.status === 200) {
+        
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
 
   const onsubmit = async (event) => {
     console.log('event',event)
